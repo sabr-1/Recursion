0301927c134121cbc989305ef8fc075d46dd40ae
sabr-1 <contact.sheikhsameer@gmail.com>
2022-06-14 14:45:56 +0530

    Updated get all subsequence of a string

--- ArrayList/getSubsequence.java
+++ ArrayList/getSubsequence.java
@@ -1,31 +1,27 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class getSubsequence {
+public class GetSubsequence {
 
-    public static ArrayList<String> getSs(String str) {
+    public static ArrayList<String> getSubsequence(String str) {
         if (str.isEmpty()) {
             ArrayList<String> sS = new ArrayList<>();
             sS.add(str);
             return sS;
         }
-        ArrayList<String> sS = getSs(str.substring(1));
-
+        ArrayList<String> sS = getSubsequence(str.substring(1));
         int size = sS.size();
         for (int i = 0; i < size; i++) {
-
-            sS.add(str.substring(0, 1) + sS.get(i));
+            sS.add(str.charAt(0) + sS.get(i));
         }
         return sS;
-
     }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String str;
         str = s.next();
-
-        System.out.println(getSs(str));
+        System.out.println(getSubsequence(str));
 
     }
 }
